82d44c156a04b30875f92578ea3d9e989fd2b552
Autuwd <2051475025@qq.com>
2025-11-23 21:45:19 +0800

    Remake V0.6

---
+++ ui/status_panel.gd
@@ -0,0 +1,27 @@
+extends HBoxContainer
+
+@export var stats: Stats
+
+@onready var health_bar: TextureProgressBar = $V/HealthBar
+@onready var eased_healrh_bar: TextureProgressBar = $V/HealthBar/EasedHealrhBar
+@onready var energy_bar: TextureProgressBar = $V/EnergyBar
+
+
+
+func _ready() -> void:
+	stats.health_changed.connect(update_health)
+	update_health()
+	
+	stats.energy_changed.connect(update_energy)
+	update_energy()
+
+func update_health() -> void:
+	var percentage := stats.health / float(stats.max_health)
+	health_bar.value = percentage
+	
+	create_tween().tween_property(eased_healrh_bar, "value", percentage, 0.3)
+
+
+func update_energy() -> void:
+	var percentage := stats.energy / stats.max_energy
+	energy_bar.value = percentage
